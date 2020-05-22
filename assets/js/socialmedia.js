e57fae5a26c6968b38ec5a6d27f832e6c43dd661
Silvan_S <silvan8124@gmail.com>
2020-05-22 16:04:06 +0530

    Refact: Making Social media hiding in index page only #Contact page change format #Hiding is seperated for index page alone Author:Silvan S

---
+++ assets/js/socialmedia.js
@@ -0,0 +1,13 @@
+/*Script to hide social media icon at particular height in index page */
+document.getElementsByClassName('social-media')[0].style.opacity=0;
+document.addEventListener('scroll',setVisibility);
+function setVisibility(){
+  var height=document.getElementsByClassName('mu-service-area')[0].clientHeight;
+  var position=window.pageYOffset;
+  var element=document.getElementsByClassName('social-media')[0];
+  if(position>=height+20){
+    element.style.opacity=1;
+  }else{
+    element.style.opacity=0;
+  }
+}
