a2f00d30be5ddd7de1e863c1407e6af915ab3813
Silvan_S <silvan8124@gmail.com>
2020-05-28 12:56:31 +0530

    Refact:Social media hiding property in Banner section Author:Silvan S

--- assets/js/socialmedia.js
+++ assets/js/socialmedia.js
@@ -1,10 +1,10 @@
 /*Script to hide social media icon at particular height in index page */
-document.getElementsByClassName('social-media')[0].style.opacity=0;
+document.getElementsByClassName('mu-top-social-nav')[0].style.opacity=0;
 document.addEventListener('scroll',setVisibility);
 function setVisibility(){
   var height=document.getElementsByClassName('mu-service-area')[0].clientHeight;
   var position=window.pageYOffset;
-  var element=document.getElementsByClassName('social-media')[0];
+  var element=document.getElementsByClassName('mu-top-social-nav')[0];
   if(position>=height+20){
     element.style.opacity=1;
   }else{
