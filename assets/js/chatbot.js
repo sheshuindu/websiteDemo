a549a558ea178ee25ce161a00a548711cffe73cc
Silvan_S <silvan8124@gmail.com>
2020-06-12 11:37:28 +0530

    Edit:Chat box Author:Silvan S

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -3,7 +3,6 @@ var json;
 
 jsonload = $.getJSON('assets/js/responses.json', function() {
     json = jsonload.responseJSON;
-    console.log('in json load');
     buildDefaultButtons();
 });
 
@@ -24,7 +23,6 @@ function buildButtons(btns) {
 
 function buildDefaultButtons() {
     defaultbtns = json.buttongroup.default;
-    console.log(defaultbtns);
     var buttonsHTML = buildButtons(defaultbtns);
     buttongrid.innerHTML = buttonsHTML;
     setOnClickHandler();
@@ -45,14 +43,12 @@ function btn_onClick(event) {
     console.log(req);
     // check for buttons
     var btns = json.buttongroup[req];
-    console.log(btns)
 
     if (!btns) {
         // check for links
         var link = json.links[req];
     } else {
         var buttonsHTML = buildButtons(btns);
-        console.log(buttonsHTML);
         buttongrid.innerHTML = buttonsHTML;
         setOnClickHandler();
         message.innerHTML = json.replies[req];
@@ -82,7 +78,6 @@ function btn_onClick(event) {
        document.getElementById(popup.id).classList.add("show");
        $(document.getElementById(popup.id)).on("click", function(e) {
             document.getElementById(popup.id).innerHTML = "";
-            console.log(e);
             e.stopPropagation();
             document.getElementById(popup.id).parentNode.removeChild(document.getElementById(popup.id));
             btn.classList.remove("popup");
@@ -98,10 +93,10 @@ function buildPopup(id, html) {
 
 document.getElementById('chatbox-button').addEventListener("click",function(){
     document.getElementsByClassName('chatbox')[0].style.visibility="visible";
-    documentdocument.getElementById('chatbox-button').visibility="hidden";
+    document.getElementById('chatbox-button').visibility="hidden";
 });
 
 document.getElementsByClassName('cancel-button')[0].addEventListener("click",function(){
     document.getElementsByClassName('chatbox')[0].style.visibility="hidden";
-    documentdocument.getElementById('chatbox-button').visibility="visible";
+    document.getElementById('chatbox-button').visibility="visible";
 });
