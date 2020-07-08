5292b5a50825698f86706cf0df5273a1345aee33
Silvan_S <silvan8124@gmail.com>
2020-07-08 20:35:51 +0530

    Feature update script

--- scripts/socialmedia.js
+++ scripts/socialmedia.js
@@ -11,3 +11,41 @@ function setVisibility(){
     element.style.opacity=0;
   }
 }
+
+/*For login form*/
+function showPasswordForm(event) {
+  event.preventDefault();
+  registerColumn.style.display = 'none';
+  emailEntryForm.style.display = 'none';
+  passwordLabel.innerHTML = 'Welcome back, ' + emailAddressField.value + '.';
+  passwordEntryForm.style.display = 'initial';
+  backButton.style.display = 'inline-block';
+  titleText.innerHTML = 'Back'
+}
+
+function showLoginForm() {
+  registerColumn.style.display = 'initial';
+  emailEntryForm.style.display = 'initial';
+  passwordLabel.innerHTML = 'Enter your email ID:';
+  passwordEntryForm.style.display = 'none';
+  backButton.style.display = 'none';
+  titleText.innerHTML = 'Log In'
+}
+
+function showConfirmation(event) {
+  event.preventDefault();
+  resetPasswordForm.style.display = 'none';
+  resetPasswordButton.style.display = 'none';
+  emailConfirmationButton.style.display = 'initial';
+  emailConfirmationText.style.display = 'initial';
+}
+
+$(document).ready(function () {
+  $(document).on('show.bs.modal', '.modal', function (event) {
+      var zIndex = 1040 + (10 * $('.modal:visible').length);
+      $(this).css('z-index', zIndex);
+      setTimeout(function () {
+          $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+      }, 0);
+  });
+});
